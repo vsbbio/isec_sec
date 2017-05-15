30f68da0de225d9f58bbd8108bd0ced35ae55519
Your Name Vinicius <vbb@outlook.com.br>
2017-05-15 15:22:15 -0300

    New wragling!

--- Data_Wrangling/ITS.py
+++ Data_Wrangling/ITS.py
@@ -4,48 +4,38 @@ import os
 pathFILE = os.getcwd()
 valid = {"sim": True, "s": True, "si": True,
              "n": False, "nao": False, "não": False}
+data = pd.DataFrame()
+nameFILE = os.listdir(pathFILE)
+myDate = input("Qual data de Referência? (DD/MM/AAAA) ")
+myDateI = list(map(int, myDate.split("/")))
 
-while True:
+if len(myDate) == 10 and myDateI[0] in range(1,32) and myDateI[1] in range(1,13) and myDateI[2] in range(2010, 2100):
 
-    if os.path.isfile(pathFILE+"\\ITS_CVM.csv") == True:
-        ans = input("Já existe uma planilha gerada, você deseja substituir [s/n]?")
-        if valid[ans] == True:
-            try:
-                os.remove(pathFILE+"\\ITS_CVM.csv")
-                continue
-            except FileNotFoundError:
-                continue
-        else:
-            print("So, have a nice day!")
-            break
+    for i in nameFILE:
 
-    try:
+        j = 0
 
-        nameFILE = os.listdir(pathFILE)
-        myDate = input("Qual data de Referência? (DD/MM/AAAA) ")
-        myDateI = list(map(int, myDate.split("/")))
+        if i.endswith(".xlsx"):
 
-        if len(myDate) == 10 and myDateI[0] in range(1,32) and myDateI[1] in range(1,13) and myDateI[2] in range(2010, 2100):
-            data = pd.DataFrame()
-
-            for i in nameFILE:
+            while j < 50:
 
                 try:
-                    dataTemp = pd.read_excel(pathFILE + "\\"+ i, converters={"VALOR_CONTA":int})
+                    dataTemp = pd.read_excel(pathFILE + "\\"+ i, converters={"VALOR_CONTA":int}, sheetname = j)
                     frames = [dataTemp, data]
                     data = pd.concat(frames)
+                    j += 1
 
                 except:
-                    print(i)
-
-            data.insert(4, "DATA_REFERENCIA", myDate)
-            data["VALOR_CONTA"] = data["VALOR_CONTA"].astype(int)
-
-            data.to_csv(pathFILE+"\\ITS_CVM.csv", sep=";",
-            index=False, doublequote=False, decimal=",", mode="a")
-            print("Arquivo txt gerado!")
-            break
+                    print("Planilha {0} e sheet {1}".format(i, j))
+                    j = 50
 
         else:
-            print("Formato da data está errado!")
-            continue   
+            pass
+
+    data.insert(4, "DATA_REFERENCIA", myDate)
+    data.to_csv(pathFILE+"\\ITS_CVM.csv", sep=";", index=False, doublequote=False, decimal=",", mode="a")
+
+    print("Arquivo txt gerado!")
+
+else:
+    print("Formato da data está errado!")
