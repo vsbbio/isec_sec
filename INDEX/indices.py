8c3250cf3dfe09f9739a0e37ba4c575ee42b4eb0
Vinicius <vinicius@isec.local>
2017-08-15 14:56:29 -0300

    Correção webscrap ipeadata

--- INDEX/indices.py
+++ INDEX/indices.py
@@ -34,6 +34,7 @@ for file in names:
     diset.append(data_temp)
 
 del(html, bs, html2, bs2, data_temp, file, item, names, txt_temp, value)
+print("CDI [ OK ]")
 
 #%% TR TR TR TR TR TR TR TR TR TR TR
 
@@ -60,6 +61,8 @@ while j < len(df.data):
     j += 1
 
 del(data_temp, df, dt_end, dt_init, html, index, j)
+print("TR [ OK ]")
+
 #%%INCC-DI INCC-DI INCC-DI
 url = "http://indiceseconomicos.secovi.com.br/indicadormensal.php?idindicador=59"
 
@@ -109,6 +112,7 @@ for item in lst_b:
     j += 1
 
 del(indext, item, j, lst_b, lst_m1, lst_m2, lstt, month, url, y)
+print("INCC-DI [ OK ]")
 
 #%%#%%INPC INPC INPC INPC INPC
 url = "http://indiceseconomicos.secovi.com.br/indicadormensal.php?idindicador=60"
@@ -159,6 +163,8 @@ for item in lst_b:
     j += 1
 
 del(indext, item, j, lst_b, lst_m1, lst_m2, lstt, month, url, y)
+print("INPC [ OK ]")
+
 #%%IGPDI IGPDI IGPDI IGPDI IGPDI IGPDI
 
 url = "http://www.ipeadata.gov.br/ExibeSerie.aspx?serid=33593&module=M"
@@ -180,11 +186,27 @@ for item in indice:
         var_temp = item.get_text()
         var.append(var_temp)
 #Removendo valores em branco
-try:
-    date.remove("")
-    var.remove("")
-except:
-    pass
+empty = date.count("")
+empty2 = var.count("")
+count = 1
+count2 = 1
+
+while count <= empty:
+    try:
+        date.remove("")
+        count +=1
+    except:
+        continue
+        count +=1
+
+while count2 <= empty2:
+    try:
+        var.remove("")
+        count2 +=1
+    except:
+        continue
+        count2 +=1
+
 
 #Editando a data para um valor INTEGER e criando uma lista de listas chamada **data**
 j = 0
@@ -194,7 +216,8 @@ while j < len(date):
     igpdiset.append(data_temp)
     j += 1
 
-del(data_temp, date, date_temp, j, url, var, var_temp)
+del(data_temp, date, date_temp, j, url, var, var_temp, count, empty, count2, empty2)
+print("IGP-DI [ OK ]")
 
 #%%IGPM IGPM IGPM IGPM
 url = "http://www.ipeadata.gov.br/ExibeSerie.aspx?serid=37796&module=M"
@@ -216,11 +239,26 @@ for item in indice:
         var_temp = item.get_text()
         var.append(var_temp)
 #Removendo valores em branco
-try:
-    date.remove("")
-    var.remove("")
-except:
-    pass
+empty = date.count("")
+empty2 = var.count("")
+count = 1
+count2 = 1
+
+while count <= empty:
+    try:
+        date.remove("")
+        count +=1
+    except:
+        continue
+        count +=1
+
+while count2 <= empty2:
+    try:
+        var.remove("")
+        count2 +=1
+    except:
+        continue
+        count2 +=1
 
 #Editando a data para um valor INTEGER e criando uma lista de listas chamada **data**
 j = 0
@@ -230,7 +268,8 @@ while j < len(date):
     igpmset.append(data_temp)
     j += 1
 
-del(data_temp, date, date_temp, j, url, var, var_temp)
+del(data_temp, date, date_temp, j, url, var, var_temp, count, empty, count2, empty2)
+print("IGP-M [ OK ]")
 
 #%%IPCA IPCA IPCA IPCA
 url = "http://www.ipeadata.gov.br/ExibeSerie.aspx?serid=36482&module=M"
@@ -252,11 +291,26 @@ for item in indice:
         var_temp = item.get_text()
         var.append(var_temp)
 #Removendo valores em branco
-try:
-    date.remove("")
-    var.remove("")
-except:
-    pass
+empty = date.count("")
+empty2 = var.count("")
+count = 1
+count2 = 1
+
+while count <= empty:
+    try:
+        date.remove("")
+        count +=1
+    except:
+        continue
+        count +=1
+
+while count2 <= empty2:
+    try:
+        var.remove("")
+        count2 +=1
+    except:
+        continue
+        count2 +=1
 
 #Editando a data para um valor INTEGER e criando uma lista de listas chamada **data**
 j = 0
@@ -266,7 +320,8 @@ while j < len(date):
     ipcaset.append(data_temp)
     j += 1
 
-del(data_temp, date, date_temp, j, url, var, var_temp)
+del(data_temp, date, date_temp, j, url, var, var_temp, empty, count, count2, empty2)
+print("IPCA [ OK ]")
 
 #%% INSERINDO DADOS NO DB
 
@@ -279,42 +334,49 @@ for item in diset:
     sql_insert = "INSERT IGNORE INTO CDI (date, VAR) VALUES({:d}, {:.4f})".format(item[0],item[1])
     c2.execute(sql_insert)
     c1.commit()
+    print("CDI_insert [ ok ]")
 
 #TR
 for item in trset:
     sql_insert = "INSERT IGNORE INTO TR (date, VAR) VALUES({:d}, {:6.2f})".format(item[0], item[1])
     c2.execute(sql_insert)
     c1.commit()
+    print("TR_insert [ ok ]")
 
 #INCCDI
 for item in inccdiset:
     sql_insert = "INSERT IGNORE INTO INCCDI (date, VAR) VALUES({:d}, {:7.3f})".format(item[0], item[1])
     c2.execute(sql_insert)
     c1.commit()
+    print("INCCDI_insert [ ok ]")
 
 #INPC
 for item in inpcset:
     sql_insert = "INSERT IGNORE INTO INPC (date, VAR) VALUES({:d}, {:7.3f})".format(item[0], item[1])
     c2.execute(sql_insert)
     c1.commit()
+    print("INPC_insert [ ok ]")
 
 #IGPDI
 for item in igpdiset:
     sql_insert = "INSERT IGNORE INTO IGPDI (date, VAR) VALUES({:d}, {:7.3f})".format(item[0], item[1])
     c2.execute(sql_insert)
     c1.commit()
+    print("IGPDI_insert [ ok ]")
 
 #IGPM
 for item in igpmset:
     sql_insert = "INSERT IGNORE INTO IGPM (date, VAR) VALUES({:d}, {:7.3f})".format(item[0], item[1])
     c2.execute(sql_insert)
     c1.commit()
+    print("IGPM_insert [ ok ]")
 
 #IPCA
 for item in ipcaset:
     sql_insert = "INSERT IGNORE INTO IPCA (date, VAR) VALUES({:d}, {:7.3f})".format(item[0], item[1])
     c2.execute(sql_insert)
     c1.commit()
+    print("IPCA_insert [ ok ]")
 
 c2.close()
 c1.close()
